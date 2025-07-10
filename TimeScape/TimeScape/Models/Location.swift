031c92ebe775f18403cd29457071ffe355447d7c
Okan Orkun <orkunokann@gmail.com>
2025-07-11 00:12:44 +0300

    Create model, dummy datas and fill assets

---
+++ TimeScape/TimeScape/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  TimeScape
+//
+//  Created by Okan Orkun on 10.07.2025.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
