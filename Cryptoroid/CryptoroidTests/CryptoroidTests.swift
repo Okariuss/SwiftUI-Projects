48fa3480512cf1e618d9f5d260276bf818e3cd48
Okan Orkun <orkunokann@gmail.com>
2025-07-27 12:55:46 +0300

    Cryptoroid project created

---
+++ Cryptoroid/CryptoroidTests/CryptoroidTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoroidTests.swift
+//  CryptoroidTests
+//
+//  Created by Okan Orkun on 27.07.2025.
+//
+
+import Testing
+@testable import Cryptoroid
+
+struct CryptoroidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
