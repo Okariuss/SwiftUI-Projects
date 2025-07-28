b0625a47e99320403b4d89fb494db514ba3f3c13
Okan Orkun <orkunokann@gmail.com>
2025-07-28 14:58:16 +0300

    Add searchbar and apply filter

--- Cryptoroid/Cryptoroid/Core/Home/ViewModels/HomeViewModel.swift
+++ Cryptoroid/Cryptoroid/Core/Home/ViewModels/HomeViewModel.swift
@@ -13,6 +13,8 @@ final class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    @Published var searchedText: String = ""
+    
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
     
@@ -21,11 +23,29 @@ final class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+        
+        $searchedText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 guard let self else { return }
                 self.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { coin in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
